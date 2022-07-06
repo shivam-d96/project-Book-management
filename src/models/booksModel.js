750e9c5eec27fc717627bd5e470134968bc746e9
ShivanandKasture <shivanandkasture1415@gmail.com>
2022-07-06 15:47:39 +0530

    createbook

--- src/models/booksModel.js
+++ src/models/booksModel.js
@@ -1,17 +1,29 @@
 const mongoose = require("mongoose");
+const moment = require('moment');
+//const Date =moment().format('YYYY-MM-DD');
 const ObjectId = mongoose.Schema.Types.ObjectId
 
 
-const bookSchema = new mongoose.Schema( {
-    title: {type:String, required: true, unique: true},
-  excerpt: {type:String, required: true}, 
-  userId: {type:ObjectId, required : true, ref : "user"},
-  ISBN: {type:String, required: true, unique: true},
-  category: {type: String, required: true},
-  subcategory:{type: [string], required: true},
-  reviews: {type:Number, default: 0, comment:{ type: Number}},
-  deletedAt: {type:Date}, 
-  releasedAt: {type:Date, required: true},
-}, {timestamps : true});
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true, unique: true },
+
+  excerpt: { type: String, required: true },
+
+  userId: { type: ObjectId, required: true, ref: "user" },
+
+  ISBN: { type: String, required: true, unique: true },
+
+  category: { type: String, required: true },
+
+  subcategory: { type: [String], required: true },
+
+  reviews: { type: Number, default: 0, comment: { type: Number } },
+
+  deletedAt: { type: Date, default: null },
+  isDeleted: {type:Boolean, default: false},
+
+  releasedAt: { type :Date, required: true },
+
+}, { timestamps: true });
 
 module.exports = mongoose.model("book", bookSchema);
