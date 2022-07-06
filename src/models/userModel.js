de638b11c051508ca532d90080b2bdf43d5e9a24
shivamd-96 <shivamd96@outlook.com>
2022-07-06 12:39:49 +0530

    userModel

---
+++ src/models/userModel.js
@@ -0,0 +1,18 @@
+ const mongoose = require("mongoose")
+ 
+   const userSchema = new mongoose.Schema ({ 
+        title: {type:String, required: true, enum:["Mr", "Mrs", "Miss"]},
+        name: {type:String, required: true},
+        phone: {type:String, required: true, unique : true},
+        email: {type:String, reqired: true, unique:true}, 
+        password: {type:String, required: true},
+        address: {
+          street: {type:String},
+          city: {type:String},
+          pincode: {type:String}
+        },
+        createdAt: {timestamp},
+        updatedAt: {timestamp}
+      },{ timestamps: true})
+
+module.exports = mongoose.model("user",userSchema)      
