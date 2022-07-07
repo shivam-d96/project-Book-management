27fa3d7e6ab0fad7c2f29bb8e82e1397a408c90b
shivamd-96 <shivamd96@outlook.com>
2022-07-07 15:59:36 +0530

    authentication

---
+++ src/middleware/auth.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken")
+const blogModel = require("../models/blogModel")
+const mongoose = require("mongoose")
+//================================================Authentication======================================================
+
+const authenticate = function (req, res, next) {
+    try {
+        const token = req.headers["x-api-key"]
+
+        if (!token) {
+            res.status(400).send({ status: false, msg: "token must be present" })
+        }
+        else {
+            const validToken = jwt.decode(token)
+            if (validToken) {
+                try {
+                    jwt.verify(token, "project-3")
+                    next()
+                }
+                catch (error) {
+                    res.status(401).send({ status: false, msg: "Invalid token" })
+                }
+            }
+            else {
+                res.status(400).send({ status: false, msg: "Invalid token" })
+            }
+        }
+    }
+    catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
+    }
+}
+
+module.exports.authenticate = authenticate
